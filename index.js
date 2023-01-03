3e8fbaf6efd389adb37efe37e59a8bddb62a6eeb
Areeb Karim <areebakarim99@gmail.com>
2023-01-03 15:07:52 -0500

    commit tree c0cd53baacb07ef5eb51a308b151be9345d25016

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = { 
+    name: "areeb",
+    streetAddress: "home"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee};
+    newObj[key] = value;
+    return newObj;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
